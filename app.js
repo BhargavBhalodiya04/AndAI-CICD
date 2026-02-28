ec3f04f801f9d01c8782a46540869e2adcd0be17
Bhargav Bhalodiya <156835982+BhargavBhalodiya04@users.noreply.github.com>
2026-02-28 10:27:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('🚀 Demo Node App is Running! V1');
+    res.send('🚀 Demo Node App is Running! V2');
 });
 
 app.get('/health', (req, res) => {
@@ -15,3 +15,4 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 
 });
+
